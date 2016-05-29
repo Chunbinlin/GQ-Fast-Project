e6c9940fbb4459a85561d616aaaacc6b1305f158
bnmandel <bmandel@eng.ucsd.edu>
2016-05-29 07:05:45 -0700

    modified operator

--- src/gqfast/codeGenerator/IntersectionOperator.java
+++ src/gqfast/codeGenerator/IntersectionOperator.java
@@ -1,22 +1,24 @@
 package gqfast.codeGenerator;
 
 import gqfast.global.Global.Optypes;
+import gqfast.global.Alias;
 
 import java.util.List;
 
 class IntersectionOperator extends Operator {
 
 	private boolean bitwiseFlag;
-	private List<Integer> indexIDs;
+	private List<Alias> aliases;
 	private List<Integer> columnIDs; 
+	private List<Integer> selections;
 	
-	public IntersectionOperator(boolean bitwiseFlag, List<Integer> indexIDs, 
-		List<Integer> columnIDs) {
+	public IntersectionOperator(boolean bitwiseFlag, List<Alias> aliases, 
+		List<Integer> columnIDs, List<Integer> selections) {
 		super(Optypes.INTERSECTION_OPERATOR);
 		this.bitwiseFlag = bitwiseFlag;
-		this.indexIDs = indexIDs;
+		this.aliases = aliases;
 		this.columnIDs = columnIDs;
-
+		this.selections = selections;
 		
 	}
 
@@ -24,13 +26,17 @@ class IntersectionOperator extends Operator {
 		return bitwiseFlag;
 	}
 
-	public List<Integer> getIndexIDs() {
-		return indexIDs;
+	public List<Alias> getAliases() {
+		return aliases;
 	}
 
 	public List<Integer> getColumnIDs() {
 		return columnIDs;
 	}
 
-	
+	public List<Integer> getSelections() {
+		return selections;
+	}
+
+
 }
