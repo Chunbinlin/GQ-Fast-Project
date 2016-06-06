0c0c8aa1401ae779c737771efbc26956c249cd67
bnmandel <bmandel@eng.ucsd.edu>
2016-06-05 22:39:29 -0700

    completed initial implementation of intersection. now requires testing

--- src/gqfast/codeGenerator/ThreadingOperator.java
+++ src/gqfast/codeGenerator/ThreadingOperator.java
@@ -6,14 +6,22 @@ import gqfast.global.Global.Optypes;
 public class ThreadingOperator extends Operator {
 
 	private Alias drivingAlias;
+	private boolean threadingAfterIntersection;
 	
-	public ThreadingOperator(Alias drivingAlias) {
+	public ThreadingOperator(Alias drivingAlias, boolean threadingAfterIntersection) {
 		super(Optypes.THREADING_OPERATOR);
 		this.drivingAlias = drivingAlias;
+		this.threadingAfterIntersection = threadingAfterIntersection;
 	}
 
 	public Alias getDrivingAlias() {
 		return drivingAlias;
 	}
 
+	public boolean isThreadingAfterIntersection() {
+		return threadingAfterIntersection;
+	}
+
+	
+	
 }
