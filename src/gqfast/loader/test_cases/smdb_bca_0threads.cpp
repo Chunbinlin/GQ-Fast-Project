028db1d99d5fa3bbf5fdf6646cb7332bf2f129a4
bnmandel <bmandel@eng.ucsd.edu>
2016-07-04 18:09:09 -0700

    debugging

--- src/gqfast/loader/test_cases/smdb_bca_0threads.cpp
+++ src/gqfast/loader/test_cases/smdb_bca_0threads.cpp
@@ -153,7 +153,7 @@ extern "C" int* smdb_bca_0threads(int** null_checks) {
 
 	benchmark_t1 = chrono::steady_clock::now();
 
-	tspan = 0;
+	tspan = chrono::duration_cast<chrono::duration<double>>(0);
 
 	int max_frag;
 
