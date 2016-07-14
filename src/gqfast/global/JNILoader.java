d5439d5c122da3c40bcfbdee575b75cd2983ccfb
bnmandel <bmandel@eng.ucsd.edu>
2016-07-13 23:16:22 -0700

    JNI Loader to load individual indexes

--- src/gqfast/global/JNILoader.java
+++ src/gqfast/global/JNILoader.java
@@ -1,31 +1,19 @@
 package gqfast.global;
 
+import gqfast.global.Global.Encodings;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class JNILoader {    
     
-	//private String pathAndFileName;
-	//private int numEncodings;
-	//private int[] colEncodings;
-	
 	private long[] discoveredColDomains;
 	private int discoveredIndexMapByteSize;
 	private int[] discoveredColByteSizes;
 	private int loaderIndexID;
 	private long discoveredIndexDomain;
 	
-	public JNILoader() {
-		//this.pathAndFileName = pathAndFileName;
-		//this.numEncodings = numEncodings;
-		//this.colEncodings = colEncodings;
-		
-		
-		//discoveredColDomains = new long[numEncodings];
-		//discoveredIndexMapByteSize = -1;
-		//discoveredColByteSizes = new int[numEncodings];
-		//loaderIndexID = -1;
-	}
+	public JNILoader() { }
 	
 	public native void cppOpenLoader();
 	public native void cppCloseLoader();
@@ -36,7 +24,82 @@ public class JNILoader {
 	static {
 		System.loadLibrary("gqfast_global_JNILoader");
 	}        
-
+	
+	public void openLoader() {
+		cppOpenLoader();
+	}
+	
+	public void closeLoader() {
+		cppCloseLoader();
+	}
+	
+	public MetaIndex loadIndex(String pathAndFileName, int numEncodings, Encodings[] colEncodings) {
+		
+		discoveredColDomains = new long[numEncodings];
+		discoveredIndexMapByteSize = -1;
+		discoveredColByteSizes = new int[numEncodings];
+		loaderIndexID = -1;
+    	discoveredIndexDomain = -1;
+		
+    	int colEncodingInts[] = new int[numEncodings];
+    	for (int i=0; i<numEncodings; i++) {
+    		
+    		Encodings currEnc = colEncodings[i];
+    		int encodingInt = 0;
+    		
+    		switch (currEnc) {
+    		case Uncompress_Array:
+    			encodingInt = 1;
+    			break;
+    		case Bit_Compress_Array:
+    			encodingInt = 2;
+    			break;
+    		case Byte_Align_Bitmap:
+    			encodingInt = 3;
+    			break;
+    		case Huffman:
+    			encodingInt = 4;
+    			break;
+    		case Uncompress_Bitmap: // TODO: implement in C++ loader
+    			encodingInt = 5;
+    			break;
+    		}
+    		colEncodingInts[i] = encodingInt;
+    		
+    	}
+    	
+    	
+    	cppLoadIndex(pathAndFileName, numEncodings, colEncodingInts);   		
+    	
+    	if (loaderIndexID >= 0) {
+    		MetaIndex myMetaIndex;
+    		List<Integer> colEncodingsList = new ArrayList<Integer>();
+    		List<Integer> colEncodedByteSizeList = new ArrayList<Integer>();
+    		List<Long> colDomains = new ArrayList<Long>();
+    			
+    		for (int i=0; i<numEncodings; i++) {
+    			colEncodingsList.add(colEncodingInts[i]);
+    			colEncodedByteSizeList.add(discoveredColByteSizes[i]);
+    			colDomains.add(discoveredColDomains[i]);  			
+    		}
+  
+    		myMetaIndex = new MetaIndex(loaderIndexID, numEncodings, discoveredIndexMapByteSize, 
+    				discoveredIndexDomain, colEncodingsList, colEncodedByteSizeList, colDomains);
+    		myMetaIndex.print();
+    		return myMetaIndex;
+    	}
+    	else {
+    		System.err.println("Error! index ID not initialized properly");
+    		return null;
+    	}
+			
+	}
+	
+	
+	
+	
+	
+	/*
     public void print (String pathAndFileName, int numEncodings, int[] colEncodings) {
     	
 		discoveredColDomains = new long[numEncodings];
@@ -69,15 +132,5 @@ public class JNILoader {
     	
       	cppCloseLoader();
     }
-    
-    public static void main(String[] args) {
-    	
-    	String p = "./gqfast/loader/pubmed/da1.csv";
-    	int n = 1;
-    	int[] c = {1};
-    	
-    	JNILoader newIndex = new JNILoader();
-    	newIndex.print(p, n, c);
-    	return;
-    }
+    */
 }
